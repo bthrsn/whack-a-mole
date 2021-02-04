d8e3c911aa74e41a875873fccd584b38150536b7
bthrsn <mikhail.lhts@gmail.com>
2021-02-04 18:44:14 +0300

    add styles and images

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,24 @@
 import createGameboard from './services/createGameboard';
 
 window.addEventListener('DOMContentLoaded', () => {
-  // Получаем нужные элементы со страницы
-  const gameBoard = document.querySelector('.gameboard'),
-        score = document.querySelector('.score'),
-        timer = document.querySelector('.timer'),
-        holes = document.querySelectorAll('.hole'),
-        moles = document.querySelectorAll('.mole');
-
+  // Объект для статуса игры, подсчета очков и времени
+  const SETTINGS = {
+    start: false,
+    points: 0,
+    time: 15 
+  }
   // Задаем количество нор для кротов
   const numberOfHoles = 9; 
   // Создаем игровое поле
-  createGameboard(gameBoard, numberOfHoles, 'hole', 'mole');
+  createGameboard('.gameboard', numberOfHoles, 'hole', 'mole');
+  
+  // Получаем остальные элементы со страницы
+  const score = document.querySelector('.score'),
+        timer = document.querySelector('.timer'),
+        holes = document.querySelectorAll('.hole'),
+        moles = document.querySelectorAll('.mole');
+  
+  console.log(holes);
+  console.log(moles);
+  console.log(timer);
 })
--- src/js/services/createGameboard.js
+++ src/js/services/createGameboard.js
@@ -1,5 +1,7 @@
-const createGameboard = (parentElement, numberOfChildElements, childClass, subchildClass) => {
-
+const createGameboard = (parentElementClass, numberOfChildElements, childClass, subchildClass) => {
+  // Получаем родительский элемент со страницы
+  const parentElement = document.querySelector(parentElementClass);
+  // Добавляем игровые ячейки по количеству детей этого элемента
   for (let i = 1; i <= numberOfChildElements; i++) {
     parentElement.insertAdjacentHTML('beforeend', 
       `<div class="${childClass} ${childClass}${i}">
